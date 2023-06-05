f8369155e57a892d685b7f6835e55b99aad2e2e0
ahlmy <ahmethilmiyoruk@gmail.com>
2023-06-05 15:34:40 +0300

    Added landmark

--- src/components/newsletter/Newsletter.jsx
+++ src/components/newsletter/Newsletter.jsx
@@ -26,70 +26,72 @@ const Newsletter = () => {
     setSuccess(true);
   };
   return (
-    <div className="newsletter-container">
-      <div>
-        {success ? (
-          <Newsletter_success
-            emailName={email}
-            setSuc={() => {
-              setSuccess(false);
-              setEmail("");
-            }}
-          />
-        ) : (
-          <div className="newsletter">
-            <div className="newsletter--content">
-              <h1 className="newsletter--content--title">Stay updated!</h1>
-              <p className="newsletter--content--desc">
-                Join 60,000+ product managers receiving monthly updates on:
-              </p>
-              <ul className="newsletter--content--list">
-                <li>
-                  <img src={icon_list} alt="icon" />
-                  <p>Product discovery and building what matters</p>
-                </li>
-                <li>
-                  <img src={icon_list} alt="icon" />
-                  <p> Measuring to ensure updates are a success</p>
-                </li>
-                <li>
-                  <img src={icon_list} alt="icon" />
-                  <p>And much more!</p>
-                </li>
-              </ul>
-              <form
-                className="newsletter--content--form"
-                onSubmit={handleSubmit}
-              >
-                <label htmlFor="email">Email address</label>
-                {blank ? <span id="asterisk">*</span> : ""}
-                <input
-                  ref={inputRef}
-                  type="email"
-                  id="email"
-                  placeholder="email@company.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <Button
-                  onMouseEnter={() => setImgRadius("0")}
-                  onMouseLeave={() => setImgRadius("25px")}
+    <main>
+      <div className="newsletter-container">
+        <div>
+          {success ? (
+            <Newsletter_success
+              emailName={email}
+              setSuc={() => {
+                setSuccess(false);
+                setEmail("");
+              }}
+            />
+          ) : (
+            <div className="newsletter">
+              <div className="newsletter--content">
+                <h1 className="newsletter--content--title">Stay updated!</h1>
+                <p className="newsletter--content--desc">
+                  Join 60,000+ product managers receiving monthly updates on:
+                </p>
+                <ul className="newsletter--content--list">
+                  <li>
+                    <img src={icon_list} alt="icon" />
+                    <p>Product discovery and building what matters</p>
+                  </li>
+                  <li>
+                    <img src={icon_list} alt="icon" />
+                    <p> Measuring to ensure updates are a success</p>
+                  </li>
+                  <li>
+                    <img src={icon_list} alt="icon" />
+                    <p>And much more!</p>
+                  </li>
+                </ul>
+                <form
+                  className="newsletter--content--form"
+                  onSubmit={handleSubmit}
                 >
-                  Subscribe to monthly newsletter
-                </Button>
-              </form>
+                  <label htmlFor="email">Email address</label>
+                  {blank ? <span id="asterisk">*</span> : ""}
+                  <input
+                    ref={inputRef}
+                    type="email"
+                    id="email"
+                    placeholder="email@company.com"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <Button
+                    onMouseEnter={() => setImgRadius("0")}
+                    onMouseLeave={() => setImgRadius("25px")}
+                  >
+                    Subscribe to monthly newsletter
+                  </Button>
+                </form>
+              </div>
+              <div className="newsletter--image--container">
+                <img
+                  className="newsletter--image"
+                  alt="signup-desktop"
+                  style={{ borderRadius: `${imgRadius}` }}
+                />
+              </div>
             </div>
-            <div className="newsletter--image--container">
-              <img
-                className="newsletter--image"
-                alt="signup-desktop"
-                style={{ borderRadius: `${imgRadius}` }}
-              />
-            </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
+    </main>
   );
 };
 export default Newsletter;
